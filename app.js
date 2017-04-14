94ef14726bdc142ace66831b1ce1e74f304b8882
Felipe Zadinello <felipezad@gmail.com>
2017-04-14 12:38:34 -0300

    Created validation if it is Palindrome

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ var express = require('express')
 var app = express()
 var path = require('path')
 
+
+app.use(express.static(__dirname + '/app'));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/app/palindrome/index.html'));
+
 })
 
 app.post('/', function(req,res){
-  res.sendStatus(200)
+
+  var result = checkPalindrom(str);
+  if(result)
+    res.sendStatus(200)
+  res.sendStatus(400)
+
 
 })
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+function checkPalindrom(str) {
+  return str == str.split('').reverse().join('');
+}

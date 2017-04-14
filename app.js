e48d9f5e866034f2041dedb3dc8e8151c34bfaeb
Felipe Zadinello <felipezad@gmail.com>
2017-04-14 12:05:20 -0300

    Created route to home

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express')
 var app = express()
+var path = require('path')
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname+'/app/palindrome/index.html'));
 })
 
+app.post('/', function(req,res){
+  res.sendStatus(200)
+
+})
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
